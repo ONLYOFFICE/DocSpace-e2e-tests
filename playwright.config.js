345bec0800c1965dd2012208e090cd2fdf80521d
AntonKashichkin <Anton.Kashichkin@onlyoffice.com>
2025-02-21 22:09:24 +0300

    plugins tests, fix old tests and return export default in playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "@playwright/test";
 import config from "./config/config.js";
 
-module.exports = defineConfig({
+export default defineConfig({
   testDir: "./tests",
   retries: 0,
   use: {
