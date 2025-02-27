81f14a67e76cf960f7c822f4957d46842418a469
Julia B <bubnovaum@gmail.com>
2025-02-27 12:07:32 +0300

    update linter rules and fix errors

--- page_objects/MainPage.js
+++ page_objects/MainPage.js
@@ -1,20 +1,22 @@
 export default class MainPage {
-    constructor(page) {
-        this.page = page;
-        this.optionsButton = page.locator('[data-testid="icon-button"].option-button-icon');
-        this.settingsMenuItem = page.getByRole('menuitem', { name: 'Settings' });
-        this.paymentsMenuItem = page.getByRole('menuitem', { name: 'Payments' });
-    }
+  constructor(page) {
+    this.page = page;
+    this.optionsButton = page.locator(
+      '[data-testid="icon-button"].option-button-icon',
+    );
+    this.settingsMenuItem = page.getByRole("menuitem", { name: "Settings" });
+    this.paymentsMenuItem = page.getByRole("menuitem", { name: "Payments" });
+  }
 
-    async navigateToSettings() {
-        await this.optionsButton.waitFor({ state: 'visible', timeout: 10000 });
-        await this.optionsButton.click();
-        await this.settingsMenuItem.click();
-    }
+  async navigateToSettings() {
+    await this.optionsButton.waitFor({ state: "visible", timeout: 10000 });
+    await this.optionsButton.click();
+    await this.settingsMenuItem.click();
+  }
 
-    async navigateToPayments() {
-        await this.optionsButton.waitFor({ state: 'visible', timeout: 10000 });
-        await this.optionsButton.click();
-        await this.paymentsMenuItem.click();
-    }
+  async navigateToPayments() {
+    await this.optionsButton.waitFor({ state: "visible", timeout: 10000 });
+    await this.optionsButton.click();
+    await this.paymentsMenuItem.click();
+  }
 }
