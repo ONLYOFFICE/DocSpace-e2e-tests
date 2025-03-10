4bebc6e50c4e31c8e0687043f85cdffc3e42bfad
Julia B <bubnovaum@gmail.com>
2025-03-10 19:15:08 +0300

    update url

--- tests/ui_tests/registrationPageOpen.spec.js
+++ tests/ui_tests/registrationPageOpen.spec.js
@@ -3,8 +3,9 @@ import config from "../../config/config.js";
 import { RegistrationPage } from "../../page_objects/site_registration_page.js";
 
 test("Verify registration page loads", async ({ page }) => {
-  const siteUrl = config.TEST_SITE_REGISTRATION_URL;
-  await page.goto(siteUrl);
+  const siteRegistrationUrl =
+    "https://www.onlyoffice.com/docspace-registration.aspx";
+  await page.goto(siteRegistrationUrl);
   const registrationPage = new RegistrationPage(page);
   await expect(registrationPage.signUpForm).toBeVisible();
 });
