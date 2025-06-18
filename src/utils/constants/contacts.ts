5bf83026715cca0bd937f64b53b586d2dbce70d7
Alexander Evdokimov <evdoki15@yandex.ru>
2025-06-18 11:14:51 +0300

    added new tests in contacts

---
+++ src/utils/constants/contacts.ts
@@ -0,0 +1,14 @@
+export const contactsActionsMenu = {
+  invite: {
+    label: "Invite",
+    submenu: {
+      docspaceAdmin: "DocSpace admin",
+      roomAdmin: "Room admin",
+      user: "User",
+      inviteAgain: "Invite again",
+    },
+  },
+  createGroup: "Create group",
+} as const;
+
+export const ADMIN_OWNER_NAME = "admin-zero admin-zero";
