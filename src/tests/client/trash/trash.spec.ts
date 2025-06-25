91fbf570b8413ac461d72b1ff710fdc626dbee89
Timofey <timofey.boyko@onlyoffice.com>
2025-06-25 16:01:03 +0500

    Update trash screenshots

--- src/tests/client/trash/trash.spec.ts
+++ src/tests/client/trash/trash.spec.ts
@@ -66,79 +66,79 @@ test.describe("Trash", () => {
       await screenshot.expectHaveScreenshot("context_menu_table_settings");
     });
 
-    // await test.step("OpenEmptyTrashDialog", async () => {
-    //   await trash.openEmptyTrashDialog("header");
-    //   await trash.openEmptyTrashDialog("table");
-    // });
+    await test.step("OpenEmptyTrashDialog", async () => {
+      await trash.openEmptyTrashDialog("header");
+      await trash.openEmptyTrashDialog("table");
+    });
 
-    // await test.step("Sort", async () => {
-    //   await trash.filter.sortButton.click();
-    //   await screenshot.expectHaveScreenshot("sort_sorted");
-    // });
+    await test.step("Sort", async () => {
+      await trash.filter.sortButton.click();
+      await screenshot.expectHaveScreenshot("sort_sorted");
+    });
 
-    // await test.step("Filter", async () => {
-    //   await trash.filter.filterButton.click();
-    //   await screenshot.expectHaveScreenshot("filter_dialog");
-    //   await page.mouse.click(1, 1);
-    // });
+    await test.step("Filter", async () => {
+      await trash.filter.filterButton.click();
+      await screenshot.expectHaveScreenshot("filter_dialog");
+      await page.mouse.click(1, 1);
+    });
 
-    // await test.step("InfoPanel", async () => {
-    //   await trash.trashTable.tableRows.first().click();
-    //   await trash.infoPanel.open();
-    //   await trash.infoPanel.hideDatePropertiesDetails();
-    //   await screenshot.expectHaveScreenshot("info_panel_view");
-    //   await trash.infoPanel.openOptions();
-    //   await screenshot.expectHaveScreenshot("info_panel_options_opened");
-    //   await trash.infoPanel.close();
-    // });
+    await test.step("InfoPanel", async () => {
+      await trash.trashTable.tableRows.first().click();
+      await trash.infoPanel.open();
+      await trash.infoPanel.hideDatePropertiesDetails();
+      await screenshot.expectHaveScreenshot("info_panel_view");
+      await trash.infoPanel.openOptions();
+      await screenshot.expectHaveScreenshot("info_panel_options_opened");
+      await trash.infoPanel.close();
+    });
 
-    // await test.step("RestoreSelector", async () => {
-    //   await trash.openRestoreSelector();
-    //   await screenshot.expectHaveScreenshot("restore_selector");
+    await test.step("RestoreSelector", async () => {
+      await trash.openRestoreSelector();
+      await screenshot.expectHaveScreenshot("restore_selector");
 
-    //   await trash.trashSelector.select("rooms");
-    //   await screenshot.expectHaveScreenshot("restore_selector_empty_rooms");
+      await trash.trashSelector.select("rooms");
+      await screenshot.expectHaveScreenshot("restore_selector_empty_rooms");
 
-    //   await trash.trashSelector.createNewItem();
-    //   await screenshot.expectHaveScreenshot(
-    //     "restore_selector_rooms_dropdown_empty",
-    //   );
-    //   await trash.trashSelector.selectCreateRoomType(roomCreateTitles.public);
-    //   await trash.trashSelector.fillNewItemName(roomCreateTitles.public);
-    //   await screenshot.expectHaveScreenshot("restore_selector_rooms_new_item");
-    //   await trash.trashSelector.acceptCreate();
-    //   await trash.trashSelector.selectFirstItem();
-    //   await trash.trashSelector.checkEmptyContainerExist();
+      await trash.trashSelector.createNewItem();
+      await screenshot.expectHaveScreenshot(
+        "restore_selector_rooms_dropdown_empty",
+      );
+      await trash.trashSelector.selectCreateRoomType(roomCreateTitles.public);
+      await trash.trashSelector.fillNewItemName(roomCreateTitles.public);
+      await screenshot.expectHaveScreenshot("restore_selector_rooms_new_item");
+      await trash.trashSelector.acceptCreate();
+      await trash.trashSelector.selectFirstItem();
+      await trash.trashSelector.checkEmptyContainerExist();
 
-    //   await trash.trashSelector.gotoRoot();
-    //   await trash.trashSelector.select("documents");
-    //   await screenshot.expectHaveScreenshot("restore_selector_empty_documents");
-    //   await trash.trashSelector.createNewFolder();
-    //   await screenshot.expectHaveScreenshot("restore_selector_folder_created");
-    // });
+      await trash.trashSelector.gotoRoot();
+      await trash.trashSelector.select("documents");
+      await screenshot.expectHaveScreenshot("restore_selector_empty_documents");
+      await trash.trashSelector.createNewFolder();
+      await screenshot.expectHaveScreenshot("restore_selector_folder_created");
+    });
 
-    // await test.step("Restore", async () => {
-    //   await trash.trashSelector.restore();
-    //   await trash.trashEmptyView.checkNoDocsTextExist();
-    //   await screenshot.expectHaveScreenshot("restore_empty_view");
-    // });
+    await test.step("Restore", async () => {
+      await trash.trashSelector.restore();
+      await trash.trashEmptyView.checkNoDocsTextExist();
+      await screenshot.expectHaveScreenshot("restore_empty_view");
+    });
 
-    // await test.step("ActionRequiredDialog", async () => {
-    //   await myDocuments.open();
-    //   await myDocuments.deleteAllDocs();
-    //   await trash.open();
-    //   await trash.openRestoreSelector();
-    //   await trash.trashSelector.select("documents");
-    //   await trash.trashSelector.createNewFolder();
-    //   await trash.trashSelector.restore();
-    //   await trash.checkActionRequiredDialogExist();
-    //   await trash.closeActionRequiredDialog();
-    //   await trash.trashSelector.close();
-    // });
+    await test.step("ActionRequiredDialog", async () => {
+      await myDocuments.open();
+      await myDocuments.deleteAllDocs();
+      await trash.open();
+      await trash.openRestoreSelector();
+      await trash.trashSelector.select("documents");
+      await trash.trashSelector.createNewFolder();
+      await trash.trashSelector.restore();
+      await trash.checkActionRequiredDialogExist();
+      await trash.closeActionRequiredDialog();
+      await trash.trashSelector.close();
+    });
 
-    // await test.step("DeleteForever", async () => {
-    //   await trash.deleteForever();
-    // });
+    await test.step("DeleteForever", async () => {
+      await trash.deleteForever();
+    });
   });
 
   test.afterAll(async () => {
