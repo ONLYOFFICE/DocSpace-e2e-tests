e1d78e1448b7fd47a9629a2d41dbd635d745a6cc
Alexander Evdokimov <evdoki15@yandex.ru>
2025-07-04 11:53:51 +0300

    fix prev commit

--- src/tests/client/settings/customization.spec.ts
+++ src/tests/client/settings/customization.spec.ts
@@ -215,6 +215,7 @@ test.describe("Customization", () => {
       await customization.removeToast();
       await customization.webOnly.click({ force: true });
       await customization.saveButton.nth(3).click();
+      await customization.removeToast();
       await customization.webOrApp.click({ force: true });
       await customization.saveButton.nth(3).click();
       await customization.removeToast();
@@ -256,6 +257,5 @@ test.describe("Customization", () => {
 
   test.afterAll(async () => {
     await api.cleanup();
-    await page.close();
   });
 });
