8781b4f82cba0b41ab8bdc3af065e99380bb25b0
Alexander Evdokimov <evdoki15@yandex.ru>
2025-07-10 18:11:20 +0300

    changed default timeout for tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -94,5 +94,5 @@ export default defineConfig({
   expect: {
     timeout: 20000, // default timeout for expect assertions (toBeVisible, toHaveText, etc.)
   },
-  timeout: 180000, // default timeout for test execution
+  timeout: 400000, // default timeout for test execution
 });
