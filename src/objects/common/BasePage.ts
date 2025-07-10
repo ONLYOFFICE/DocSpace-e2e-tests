6022049da9f237afa83179498645fdd72cccac6f
Alexander Evdokimov <evdoki15@yandex.ru>
2025-07-10 15:09:30 +0300

    small changes in base objects

--- src/objects/common/BasePage.ts
+++ src/objects/common/BasePage.ts
@@ -1,16 +1,20 @@
 import { Locator, Page } from "@playwright/test";
 import BaseArticle from "./BaseArticle";
 import BaseToast from "./BaseToast";
+import Network from "./Network";
 
 export default class BasePage {
   protected page: Page;
   protected article: BaseArticle;
   protected toast: BaseToast;
+  protected network: Network;
 
   constructor(page: Page) {
     this.page = page;
     this.article = new BaseArticle(page);
     this.toast = new BaseToast(page);
+
+    this.network = Network.getInstance(this.page);
   }
 
   protected get optionsButton(): Locator {
@@ -45,10 +49,18 @@ export default class BasePage {
     await this.optionsButton.waitFor({ state: "visible", timeout: 10000 });
     await this.optionsButton.click();
     await this.page.waitForLoadState("load");
+    await this.page.waitForLoadState("load");
     await this.settingsMenuItem.click();
   }
 
   async navigateToArticle(title: string) {
     await this.article.navigate(title);
+  }
+
+  async navigateToPayments() {
+    await this.optionsButton.waitFor({ state: "visible", timeout: 10000 });
+    await this.optionsButton.click();
+    await this.page.waitForLoadState("load");
+    await this.paymentsMenuItem.click();
   }
 }
