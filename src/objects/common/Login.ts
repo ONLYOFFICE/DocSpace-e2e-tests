77e11a468408da5afff7d3eadc552a1eab4e5776
Alexander Evdokimov <evdoki15@yandex.ru>
2025-07-10 18:11:52 +0300

    fix navigation

--- src/objects/common/Login.ts
+++ src/objects/common/Login.ts
@@ -1,6 +1,7 @@
 import { Page, Locator } from "@playwright/test";
 
 import config from "../../../config";
+import Network from "./Network";
 
 export class Login {
   page: Page;
@@ -10,6 +11,7 @@ export class Login {
   emailInput: Locator;
   passwordInput: Locator;
   loginButton: Locator;
+  network: Network;
 
   constructor(page: Page, portalDomain: string) {
     this.page = page;
@@ -17,12 +19,14 @@ export class Login {
     this.emailInput = page.locator("#login_username");
     this.passwordInput = page.locator("#login_password");
     this.loginButton = page.locator("#login_submit");
+    this.network = Network.getInstance(page);
   }
 
   async loginToPortal() {
     await this.page.goto(`https://${this.portalDomain}`, {
-      waitUntil: "networkidle",
+      waitUntil: "load",
     });
+    await this.network.waitForNetworkIdle();
     await this.emailInput.waitFor({ state: "visible" });
     await this.passwordInput.waitFor({ state: "visible" });
 
@@ -30,10 +34,8 @@ export class Login {
     await this.passwordInput.fill(config.DOCSPACE_ADMIN_PASSWORD);
 
     await this.loginButton.click();
-
-    await this.page.waitForURL(/.*rooms\/shared\/filter.*/, {
-      waitUntil: "networkidle",
-    });
+    await this.page.waitForURL(/.*rooms\/shared\/filter.*/);
+    await this.network.waitForNetworkIdle();
   }
 }
 
