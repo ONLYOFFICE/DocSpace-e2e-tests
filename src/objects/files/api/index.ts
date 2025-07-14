918042e16584f0f4d74c848d4eccc3c5ee1e3bcc
Alexander Evdokimov <evdoki15@yandex.ru>
2025-07-14 10:20:30 +0300

    Added wait response in api helpers

--- src/objects/files/api/index.ts
+++ src/objects/files/api/index.ts
@@ -4,9 +4,7 @@ export const waitForGetFilesResponse = (page: Page) => {
   return page.waitForResponse((response) => {
     return (
       response.url().includes("api/2.0/files") &&
-      response.url().includes("?count") &&
-      response.request().method() === "GET" &&
-      response.status() === 200
+      response.request().method() === "GET"
     );
   });
 };
