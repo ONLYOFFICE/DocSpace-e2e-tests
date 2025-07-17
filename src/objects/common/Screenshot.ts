0dad5a71847d04cd5e05fa012f1580f561ccc4c4
Alexander Evdokimov <evdoki15@yandex.ru>
2025-07-17 16:02:30 +0300

    Screenshot: set original viewport after screenshot with fullPage flag

--- src/objects/common/Screenshot.ts
+++ src/objects/common/Screenshot.ts
@@ -64,6 +64,8 @@ class Screenshot {
 
     await waitUntilReady(this.page);
 
+    const originalViewport = this.page.viewportSize();
+
     if (this.options.fullPage) {
       await this.setViewportSize();
     }
@@ -71,6 +73,10 @@ class Screenshot {
     const screenshotName = this.getScreenshotName(comment);
 
     await this.tryScreenshot(this.options.maxAttempts!, screenshotName);
+
+    if (this.options.fullPage && originalViewport) {
+      await this.page.setViewportSize(originalViewport);
+    }
   }
 
   private async tryScreenshot(maxAttempts: number, screenshotName: string) {
