f61445c9b716b3caadcf650314c3e75e3c021ce8
Natalya Dunina <natalya.dunina@onlyoffice.com>
2025-07-17 14:38:14 +0300

    Added tests for tags in Rooms

--- src/objects/common/BaseFilter.ts
+++ src/objects/common/BaseFilter.ts
@@ -14,6 +14,8 @@ export const FILTER = {
   BUTTON: "#filter-button",
   DIALOG: "#modal-dialog",
   APPLY_BUTTON: "#filter_apply-button",
+  CANCEL_BUTTON: "#filter_cancel-button",
+  CLEAR_BUTTON: ".additional-icons-container",
 } as const;
 
 export const SEARCH = {
@@ -42,6 +44,10 @@ class BaseFilter {
   get sortButton() {
     return this.page.locator(SORT.BUTTON);
   }
+ 
+  get sortBySizeOption() {
+    return this.page.locator(SORT.BY_SIZE);
+  }
 
   get filterButton() {
     return this.page.locator(FILTER.BUTTON);
@@ -53,6 +59,10 @@ class BaseFilter {
 
   get filterApplyButton() {
     return this.page.locator(FILTER.APPLY_BUTTON);
+  }
+
+  get filterCancelButton() {
+    return this.page.locator(FILTER.CANCEL_BUTTON);
   }
 
   get searchInput() {
@@ -107,9 +117,18 @@ class BaseFilter {
     await expect(this.filterDialog).not.toBeVisible();
   }
 
-  protected async clearFilter() {
+  async cancelFilter() {
+    await this.filterCancelButton.click();
+    await expect(this.filterDialog).not.toBeVisible();
+  }
+
+  async clearFilter() {
     await this.emptyViewClearButton.click();
     await expect(this.emptyViewContainer).not.toBeVisible();
+  }
+  async clearFilterDialog() {
+    await this.page.locator(FILTER.CLEAR_BUTTON).click();
+    await expect(this.filterDialog).toBeVisible();
   }
 
   protected async fillSearchInputAndCheckRequest(searchValue: string) {
