2223be0f059a16071a1b980221084c9fd1605489
Alexander Evdokimov <evdoki15@yandex.ru>
2025-07-24 12:14:41 +0300

    test: add API response wait for folder move operation

--- src/objects/files/api/index.ts
+++ src/objects/files/api/index.ts
@@ -4,7 +4,9 @@ export const waitForGetFilesResponse = (page: Page) => {
   return page.waitForResponse((response) => {
     return (
       response.url().includes("api/2.0/files") &&
-      response.request().method() === "GET"
+      response.url().includes("?count") &&
+      response.request().method() === "GET" &&
+      response.status() === 200
     );
   });
 };
