d995fdc81d0c48e311d2ae579fb07ed29ab5252e
Alexander Evdokimov <evdoki15@yandex.ru>
2025-08-04 13:05:30 +0300

    fix: of create dropdown opening

--- src/objects/common/BaseNavigation.ts
+++ src/objects/common/BaseNavigation.ts
@@ -41,10 +41,8 @@ class BaseNavigation {
   }
 
   async openCreateDropdown() {
+    await expect(this.headerAddButton).toBeVisible();
     await expect(async () => {
-      await expect(this.headerAddButton).toBeVisible({
-        timeout: 500,
-      });
       await this.clickAddButton();
       await this.contextMenu.checkMenuExists(500);
     }).toPass();
