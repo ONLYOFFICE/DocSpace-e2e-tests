d3262a493662f760cae8da9bc342eea01e8c9e5f
AntonKashichkin <Anton.Kashichkin@onlyoffice.com>
2025-08-26 12:14:11 +0300

    fix conflict

--- src/objects/common/Screenshot.ts
+++ src/objects/common/Screenshot.ts
@@ -1,5 +1,4 @@
 import { expect, Page, PageScreenshotOptions } from "@playwright/test";
-import { waitUntilReady } from "@/src/utils";
 
 type ScreenshotOptions = {
   screenshotDir: string;
@@ -65,8 +64,6 @@ class Screenshot {
     safe: boolean = true,
     playwrightOptions?: PlaywrightScreenshotOptions,
   ) {
-    await waitUntilReady(this.page);
-
     if (safe) {
       await this.page.mouse.move(1, 1);
     }
