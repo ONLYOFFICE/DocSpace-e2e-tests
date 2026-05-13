a27d782f9bbe837f01e902646e812625e6105c00
Natalya Dunina <natalya.dunina@onlyoffice.com>
2026-05-13 10:36:41 +0300

    Update newRoomButton locator to use getByRole

--- src/objects/rooms/RoomsArticle.ts
+++ src/objects/rooms/RoomsArticle.ts
@@ -1,15 +1,13 @@
 import { expect, Page } from "@playwright/test";
 import BaseArticle from "../common/BaseArticle";
 
-const NEW_ROOM_BUTTON = "create_new_room_button";
-
 class RoomsArticle extends BaseArticle {
   constructor(page: Page) {
     super(page);
   }
 
   private get newRoomButton() {
-    return this.articleContainer.getByTestId(NEW_ROOM_BUTTON);
+    return this.articleContainer.getByRole("button", { name: "New room" });
   }
   private get dialogHeader() {
     return this.page.getByTestId("aside-header").last();
